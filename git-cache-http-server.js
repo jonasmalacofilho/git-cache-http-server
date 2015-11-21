03e6a9d0a01a51d7f6b57a869e43d188dac1371d
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-11-21 20:03:01 -0200

    Messy: fetch/clone first

--- git-cache-http-server.js
+++ git-cache-http-server.js
@@ -1,5 +1,7 @@
-var __http = require("http");
 var __child_process = require("child_process");
+var __fs = require("fs");
+var __http = require("http");
+var __mkdir_p = require("mkdir-p");
 
 var listen = 8080;
 var cacheLocation = "/tmp/var/cache/git/";
@@ -26,29 +28,37 @@ __http.createServer(function (req, res) {
 
 	var remote = parts[1];
 	// TODO don't allow arbitrary paths to be constructed with ".."
-	var local = cacheLocation + remote + ".git";
+	var local = cacheLocation + remote + ".git/";
 	console.log("remote: " + remote);
 	console.log("local: " + local);
 
 	if (parts[3] != null) {
-		// TODO check our cache
-		// TODO fetch or clone
-		
-		// respond
-		res.statusCode = 200;
-		res.setHeader("Content-Type", "application/x-git-upload-pack-advertisement");
-		res.setHeader("Cache-Control", "no-cache");
-		res.write("001e# service=git-upload-pack\n0000");
-		var up = __child_process.spawn("git-upload-pack", ["--stateless-rpc", "--advertise-refs", local]);
-		up.stdout.pipe(res);
-		up.stderr.on("data", function (data) {
-			console.log("git-upload-pack: stderr: " + data);
-		});
-		up.on("exit", function (code) {
-			console.log("git-upload-pack: exit code: " + code);
-			if (code != 0) {
-				res.end();
+		__fs.stat(local + "objects", function (err, stats) {
+			// fetch or clone
+			if (err) {
+				console.log("mirror: clonning");
+				__mkdir_p.sync(local);
+				__child_process.execSync("git clone --quiet --mirror https://" + remote + " " + local);
+			} else {
+				console.log("mirror: fetching");
+				__child_process.execSync("git -C " + local + " fetch --quiet --force");
 			}
+			// respond
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "application/x-git-upload-pack-advertisement");
+			res.setHeader("Cache-Control", "no-cache");
+			res.write("001e# service=git-upload-pack\n0000");
+			var up = __child_process.spawn("git-upload-pack", ["--stateless-rpc", "--advertise-refs", local]);
+			up.stdout.pipe(res);
+			up.stderr.on("data", function (data) {
+				console.log("git-upload-pack: stderr: " + data);
+			});
+			up.on("exit", function (code) {
+				console.log("git-upload-pack: exit code: " + code);
+				if (code != 0) {
+					res.end();
+				}
+			});
 		});
 	} else {
 		res.statusCode = 200;
