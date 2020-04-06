fc14a8ff7cb3363841523b0751d6c620ed2424a4
Jonas Malaco <jonas@protocubo.io>
2020-04-06 01:50:19 -0300

    Replace trace with println

--- src/Main.hx
+++ src/Main.hx
@@ -1,3 +1,4 @@
+import Sys.println;
 import js.node.*;
 import js.node.http.*;
 
@@ -60,7 +61,7 @@ class Main {
 
 	static function authenticate(params, infos, callback:IncomingMessage->Void)
 	{
-		trace('INFO: authenticating on the upstream repo $infos');
+		println('INFO: authenticating on the upstream repo $infos');
 		var req:ClientRequest;
 		if (proxyAgent == null) {
 			req = Https.request('https://${params.repo}/info/refs?service=${params.service}', callback);
@@ -84,25 +85,25 @@ class Main {
 	{
 		if (!updatePromises.exists(local)) {
 			updatePromises[local] = new Promise(function(resolve, reject) {
-				trace('INFO: updating: fetching from $infos');
+				println('INFO: updating: fetching from $infos');
 				fetch(remote, local, function (ferr, stdout, stderr) {
 					if (ferr != null) {
-						trace("WARN: updating: fetch failed");
-						trace(stdout);
-						trace(stderr);
-						trace("WARN: continuing with clone");
+						println("WARN: updating: fetch failed");
+						println(stdout);
+						println(stderr);
+						println("WARN: continuing with clone");
 						clone(remote, local, function (cerr, stdout, stderr) {
 							if (cerr != null) {
-								trace(stdout);
-								trace(stderr);
+								println(stdout);
+								println(stderr);
 								resolve('ERR: git clone exited with non-zero status: ${cerr.code}');
 							} else {
-								trace("INFO: updating via clone: success");
+								println("INFO: updating via clone: success");
 								resolve(null);
 							}
 						});
 					} else {
-						trace("INFO: updating via fetch: success");
+						println("INFO: updating via fetch: success");
 						resolve(null);
 					}
 				});
@@ -116,11 +117,11 @@ class Main {
 				return Promise.reject(err);
 			});
 		} else {
-			trace("INFO: reusing existing promise");
+			println("INFO: reusing existing promise");
 		}
 		return updatePromises[local]
 		.then(function(nothing:Dynamic) {
-			trace("INFO: promise fulfilled");
+			println("INFO: promise fulfilled");
 			callback(null);
 		}, function(err:Dynamic) {
 			callback(err);
@@ -130,7 +131,7 @@ class Main {
 	static function handleRequest(req:IncomingMessage, res:ServerResponse)
 	{
 		try {
-			trace('${req.method} ${req.url}');
+			println('${req.method} ${req.url}');
 			var params = getParams(req);
 			var infos = '${params.repo}';
 			if (params.auth != null)
@@ -162,8 +163,8 @@ class Main {
 				if (params.isInfoRequest) {
 					update(remote, local, infos, function (err) {
 						if (err != null) {
-							trace('ERR: $err');
-							trace(haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
+							println('ERR: $err');
+							println(haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
 							res.statusCode = 500;
 							res.end();
 							return;
@@ -174,11 +175,11 @@ class Main {
 						res.write("001e# service=git-upload-pack\n0000");
 						var up = ChildProcess.spawn(params.service, ["--stateless-rpc", "--advertise-refs", local]);
 						up.stdout.pipe(res);
-						up.stderr.on("data", function (data) trace('${params.service} stderr: $data'));
+						up.stderr.on("data", function (data) println('${params.service} stderr: $data'));
 						up.on("exit", function (code) {
 							if (code != 0)
 								res.end();
-							trace('INFO: ${params.service} done with exit $code');
+							println('INFO: ${params.service} done with exit $code');
 						});
 					});
 				} else {
@@ -192,17 +193,17 @@ class Main {
                                         else
                                                 req.pipe(up.stdin);
 					up.stdout.pipe(res);
-					up.stderr.on("data", function (data) trace('${params.service} stderr: $data'));
+					up.stderr.on("data", function (data) println('${params.service} stderr: $data'));
 					up.on("exit", function (code) {
 						if (code != 0)
 							res.end();
-						trace('${params.service} done with exit $code');
+						println('${params.service} done with exit $code');
 					});
 				}
 			});
 		} catch (err:Dynamic) {
-			trace('ERROR: $err');
-			trace(haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
+			println('ERROR: $err');
+			println(haxe.CallStack.toString(haxe.CallStack.exceptionStack()));
 			res.statusCode = 500;
 			res.end();
 		}
@@ -235,8 +236,8 @@ Options:
 		if (listenPort == null || listenPort < 1 || listenPort > 65535)
 			throw 'Invalid port number: ${options["--port"]}';
 
-		trace('INFO: cache directory: $cacheDir');
-		trace('INFO: listening to port: $listenPort');
+		println('INFO: cache directory: $cacheDir');
+		println('INFO: listening to port: $listenPort');
 
 		var env = Sys.environment();
 		var proxyUrl = env["http_proxy"];
