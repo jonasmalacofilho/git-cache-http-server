e5a345a5430a91e99fb88bbff213e038aa2aaf0b
Jonas Malaco <jonas@protocubo.io>
2020-09-14 12:01:54 -0300

    oxidize: clean up binary module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use git_cache_http_server::first_attempt;
+use eyre::{Result, WrapErr};
+
+use structopt::StructOpt;
+
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
+
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::path::PathBuf;
-use structopt::StructOpt;
+
+use git_cache_http_server::first_attempt;
 
 /// A caching Git HTTP server.
 ///
@@ -28,32 +33,35 @@ struct Opt {
 
 async fn http_service(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     eprintln!("{} {}", req.method(), req.uri());
+
     let auth = first_attempt::parse_authorization(req.headers()).expect("failed to parse auth");
     if let Some(basic_auth) = auth {
         eprintln!("requested by {}", basic_auth.safe_username());
     }
+
     let uri_info = first_attempt::parse_uri(req.uri()).expect("failed to parse request URI");
     eprintln!(
         "requesting service {} from repository {}",
         uri_info.service, uri_info.repository
     );
+
     Ok(Response::new(Body::empty()))
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let opt = Opt::from_args();
     // TODO get proxy settings from the environment
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], opt.port));
-    let http_service =
-        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(http_service)) });
-    let server = Server::bind(&addr).serve(http_service);
+    let service = make_service_fn(|_| async { Ok::<_, Infallible>(service_fn(http_service)) });
+    let addr = SocketAddr::from(([0, 0, 0, 0], opt.port));
+    let server = Server::try_bind(&addr)
+        .wrap_err_with(|| format!("could not bind to {}", addr))?
+        .serve(service);
 
     eprintln!("INFO: cache directory: {}", opt.cache_dir.to_string_lossy());
     eprintln!("INFO: port number: {}", server.local_addr().port());
+    eprintln!("INFO: starting server");
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    server.await.wrap_err("server terminated unexpectedly")
 }
